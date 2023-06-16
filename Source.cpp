cb4a7ab05a7757724d78920d7c19176163e74e99
Kosmo <The_Kosmo@outlook.com>
2023-06-17 02:55:25 +0500

    Полностью работает

--- Source.cpp
+++ Source.cpp
@@ -20,6 +20,9 @@ public:
 
 	// Для конкретной вершины метод выводит в вектор «вершины» все вершины, из которых можно дойти по ребру в данную
 	virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const = 0; 
+	
+	virtual void display() = 0;
+	virtual std::vector<std::vector<int>> get_vec() const = 0;
 };
 
 
@@ -28,14 +31,14 @@ class MatrixGraph : public IGraph
 private:
 	std::vector<std::vector<int>> matrix;
 	
-
 	// i[j] = -(j[i])
+	// Exemple
 	//     1   2   3  4
 	//   +-------------
-	// 1 | 0  -1  -1  0
-	// 2 | 1   0  -1 -2
-	// 3 | 1   1   0 -1
-	// 4 | 0   2   1  0 
+	// 1 | 0   1   1  0
+	// 2 | -1  0   1  2
+	// 3 | -1 -1   0  1
+	// 4 | 0  -2  -1  0 
 
 public:
 
@@ -44,14 +47,14 @@ public:
 		matrix = _oth->matrix;
 	};
 	MatrixGraph(IGraph* obj) {};
-
+	MatrixGraph(class ListGraph& oth);
 	MatrixGraph& operator=(const MatrixGraph& oth)
 	{
 		this->matrix = oth.matrix;
 	}
 	MatrixGraph& operator=(class ListGraph& oth);
 
-	std::vector<std::vector <int>> get_vec() const 
+	std::vector<std::vector<int>> get_vec() const override
 	{
 		return matrix;
 	}
@@ -66,7 +69,7 @@ public:
 		if (from > matrix.size())
 		{
 			matrix.resize(from);
-			for (int i = 0; i < from; i++)
+			for (int i = 0; i < matrix.size(); i++)
 			{
 				matrix[i].resize(from);
 			}
@@ -75,7 +78,7 @@ public:
 		if (to > matrix.size())
 		{
 			matrix.resize(to);
-			for (int i = 0; i < to; i++)
+			for (int i = 0; i < matrix.size(); i++)
 			{
 				matrix[i].resize(to);
 			}
@@ -84,9 +87,14 @@ public:
 		//Будет логическая ошибка при таком сценарии:
 		// 1 -> 2 | matrix[1][2]++; matrix[2][1]--;
 		// 1 <- 2 | matrix[1][2]--; matrix[2][1]++;
+		
+		//convert to index vector
+		//num Vertex = index + 1
+		from--;
+		to--;
 
-		matrix[from][to] ++;
-		matrix[to][from] --;
+		matrix[from][to]++;
+		matrix[to][from]--;
 	}
 
 	int VerticesCount() const override
@@ -127,7 +135,18 @@ public:
 		}
 	}
 
-	
+	void display() override 
+	{
+		for (int i = 0; i < matrix.size(); i++)
+		{
+			std::cout << i+1 << "| ";
+			for (int j = 0; j < matrix.size(); j++)
+			{
+				std::cout << matrix[i][j] << '\t';
+			}
+			std::cout << std::endl;
+		}
+	}
 
 	~MatrixGraph() {	};
 };
@@ -154,6 +173,20 @@ public:
 		
 	}
 	~ListGraph() {	 };
+	ListGraph(const MatrixGraph& anoth)
+	{
+		const std::vector<std::vector <int>> another_vec = anoth.get_vec();
+		list.resize(another_vec.size());
+		for (int i = 0; i < another_vec.size(); i++)
+		{
+			for (int j = 0; j < another_vec[i].size(); j++)
+			{
+				if (another_vec[i][j] > 0)
+					for (int k = another_vec[i][j];
+						k > 0; k--)list[i].push_back(j);
+			}
+		}
+	}
 
 	ListGraph& operator=(const MatrixGraph& anoth)
 	{
@@ -163,12 +196,18 @@ public:
 		{
 			for (int j = 0; j < another_vec[i].size(); j++)
 			{
-				if (another_vec[i][j] < 0) list[i].push_back(another_vec[i][j]);
+				if (another_vec[i][j] > 0)
+					for (int k = another_vec[i][j];
+						k > 0; k--)list[i].push_back(j);
 			}
 		}
 		return *this;
 	}
-
+	ListGraph& operator=(const ListGraph& oth)
+	{
+		this->list = oth.list;
+	}
+	
 	void AddEdge(int from, int to) override
 	{
 		if (from < 0 || to < 0)
@@ -194,12 +233,18 @@ public:
 		{
 			list.resize(from);
 		}
-		list[from].push_back(to);
-		if (to > list[from].size())
+		if (to > list.size())
 		{
-			list[from].resize(to);
+			list.resize(to);
 		}
-		list[to].push_back(from);
+
+		//convert to index vector
+		//num Vertex = index + 1
+		from--;
+		to--;
+
+		list[from].push_back(to);
+		//list[to].push_back(from);
 	}
 
 	int VerticesCount() const override
@@ -234,7 +279,24 @@ public:
 		
 	}
 
-	std::vector<std::vector<int>> get_list() const 
+	void display() override
+	{
+		for (int i = 0; i < list.size(); i++)
+		{
+			std::cout << i+1 << "|";
+			for (int j = 0; j < list[i].size(); j++)
+			{
+				std::cout << list[i][j] + 1;
+				if (j != list[i].size() - 1)
+				{
+					std::cout << "->";
+				}
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	std::vector<std::vector<int>> get_vec() const override
 	{
 		return list;
 	}
@@ -245,7 +307,7 @@ MatrixGraph& MatrixGraph::operator=(class ListGraph& oth)
 {
 	if (&oth == nullptr) { return *this; }
 
-	const std::vector< std::vector <int>> oth_Vec = oth.get_list();
+	const std::vector< std::vector <int>> oth_Vec = oth.get_vec();
 
 	if (oth_Vec.size() <= 0){	return *this;	}
 
@@ -262,10 +324,83 @@ MatrixGraph& MatrixGraph::operator=(class ListGraph& oth)
 	}
 	return *this;
 }
+MatrixGraph::MatrixGraph(class ListGraph& oth)
+{
+	if (&oth == nullptr) { return; }
 
+	const std::vector< std::vector <int>> oth_Vec = oth.get_vec();
+
+	if (oth_Vec.size() <= 0) { return ; }
+
+	matrix.resize(oth_Vec.size());
+
+	for (int i = 0; i < oth_Vec.size(); i++)
+	{
+		matrix[i].resize(oth_Vec.size());
+		for (int j = 0; j < oth_Vec[i].size(); j++)
+		{
+			matrix[i][(oth_Vec[i][j])]++;
+			//matrix[(oth_Vec[i][j])][i]--;
+		}
+	}
+	for (int i = 0; i < oth_Vec.size(); i++)
+	{
+		for (int j = 0; j < oth_Vec[i].size(); j++)
+		{
+
+			matrix[(oth_Vec[i][j])][i]--;
+		}
+	}
+}
 
 int main(int, char**)
 {
+	MatrixGraph g1;
+
+	// Example
+	//     1   2   3   4
+	//   +--------------
+	// 1 |  0   1   1  0
+	// 2 | -1   0   1  2
+	// 3 | -1  -1   0  1
+	// 4 |  0  -2  -1  0 
+	
+	g1.AddEdge(1, 2);
+	g1.AddEdge(1, 3);
+	g1.AddEdge(2, 3);
+	g1.AddEdge(2, 4);
+	g1.AddEdge(2, 4);
+	g1.AddEdge(3, 4);
+	//Do that
+
+	g1.display();
+	
+
+	system("Pause");
+	system("cls");
+
+
+	ListGraph g2 = g1;
+	//Check MatrixGraph -> ListGraph convert
+	/*
+	g2.AddEdge(1, 2);
+	g2.AddEdge(1, 3);
+	g2.AddEdge(2, 3);
+	g2.AddEdge(2, 4);
+	g2.AddEdge(2, 4);
+	g2.AddEdge(3, 4);
+	*/
+
+	g2.display();
+
+	system("Pause");
+	system("cls");
+	
+	//Check ListGraph -> MatrixGraph convert
+	// Same graph
+	MatrixGraph g3 = g2;
+
+	g3.display();
 
 	return 0;
 }
